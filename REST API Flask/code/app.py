63810ff89caf4f1cf9a30761c9aafefac681fed6
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-08-10 20:00:10 -0500

    Implement put

--- REST API Flask/code/app.py
+++ REST API Flask/code/app.py
@@ -1,6 +1,6 @@
 from email import message
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -14,6 +14,16 @@ jwt = JWT(app, authenticate, identity) #creates a new endpoint cal /auth
 items = []
 """ This class is a resource"""
 class Item(Resource):
+    parser = reqparse.RequestParser()
+        # price argument cannot be blank
+        # if there's more arguments we need to created
+        # you put the parser here to be able to access later
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help = "This field cannot be left blank!"
+    )
+
     @jwt_required()
     def get(self, name):
         """for item in items:
@@ -29,7 +39,8 @@ class Item(Resource):
         if next(filter(lambda x: x['name'] == name, items), None) is not None:
             return ({'message': "An item with name '{}' already exists.".format(name)}, 400)
 
-        data = request.get_json()
+        # data = request.get_json()
+        data = Item.parser.parse_args()
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201 #This code is for CREATED
@@ -38,6 +49,20 @@ class Item(Resource):
         global items
         items = list(filter(lambda x: x['name'] != name, items)) #create a new list with all the element except the one that match
         return {'message': 'Item deleted'}
+
+    def put(self, name):
+        
+        data = Item.parser.parse_args()
+        #data = request.get_json()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            # the item does not exist -- new
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            # update the item
+            item.update(data)
+        return item
 
 class ItemList(Resource):
     def get(self):
